760a517443674a320fb719733b7f9b2fc69b0940
LanreSam <ezechelanre@gmail.com>
2024-08-16 14:13:37 +0100

    added random quotes dynamically using the map function.

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -14,6 +14,13 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    "The only limit to our realization of tomorrow is our doubts of today.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "In the end, we will remember not the words of our enemies, but the silence of our friends."
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +29,11 @@ class _QuoteListState extends State<QuoteList> {
         title: Text('Awesome Quotes'),
         backgroundColor: Colors.pinkAccent,
         centerTitle: true,
+      ),
+      body: Column(
+        children: quotes.map((singleQuote) {
+          return Text(singleQuote);
+        }).toList(),
       ),
     );
   }
