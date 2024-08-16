3bb2729bff311a95f173315fc3b7e398ba570196
LanreSam <ezechelanre@gmail.com>
2024-08-16 14:02:04 +0100

    fixed the appbar

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -16,6 +16,13 @@ class QuoteList extends StatefulWidget {
 class _QuoteListState extends State<QuoteList> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        backgroundColor: Colors.pinkAccent,
+        centerTitle: true,
+      ),
+    );
   }
 }
