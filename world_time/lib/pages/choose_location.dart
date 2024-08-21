a3285354139c1f1b0e0e62ef5868e4472006c13a
LanreSam <ezechelanre@gmail.com>
2024-08-21 10:57:27 +0100

    Working on the choose location screen.

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -11,6 +11,18 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple[900],
+        foregroundColor: Colors.white,
+        title: Text('Choose Location'),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+        ),
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.grey[200],
       body: Text('Choose Location Screen'),
     );
   }
