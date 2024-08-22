38f89cc6250f42bb28cb19c9486363a1c1c798c8
LanreSam <ezechelanre@gmail.com>
2024-08-22 12:38:35 +0100

    Making changes

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -20,43 +20,48 @@ class _HomeState extends State<Home> {
       setState(() {
         data = arguments;
         print(data);
+
+        //set background image
+        String bgImage = data['isDayTime'] ? 'day.svg' : 'night.svg';
       });
     }
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-        child: SafeArea(
-          child: Column(
-            children: [
-              TextButton.icon(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/location');
-                }, 
-                icon: Icon(Icons.edit_location),
-                label: Text("Edit Location"),
-              ),
-              SizedBox(height: 20.0,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data['location'],
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20.0,),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 66.0,
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+          child: SafeArea(
+            child: Column(
+              children: [
+                TextButton.icon(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/location');
+                  }, 
+                  icon: Icon(Icons.edit_location),
+                  label: Text("Edit Location"),
                 ),
-              ),
-            ],
+                SizedBox(height: 20.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
